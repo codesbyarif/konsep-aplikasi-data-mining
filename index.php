124fa906d61b7bbd4ea485feaa0903beaef1b302
unknown <codeswitharif@gmail.com>
2023-11-16 20:10:34 +0700

    perintahTugas4

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
 <?php 
-
-    // variabel dan tipe data
-    // variabel = digunakan untuk menyimpan data
-    // Aturan variabel 'Tidak boleh diawali dengan angka,tapi boleh mengandung angka'
     $nama = "Eka";
+
+    echo "Hallo,Selamat datang $nama </br>"; #petik_1
+    echo 'Hallo,Selamat datang $nama </br>'; #petik_2
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Belajar PHP</title>
-</head>
-<body>
-
-    <?php echo "<h1>Selamat Datang Nama anda</h1>";?>
-
-</body>
-</html>
